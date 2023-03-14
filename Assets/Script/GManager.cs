6b210fa96199438776366cc54ce60c8d8e804b15
ludovicmantovani <ludovic.mantov@gmail.com>
2023-03-14 16:42:55 +0100

    [UPDATE] : Refactoring model creation

--- Assets/Script/GManager.cs
+++ Assets/Script/GManager.cs
@@ -12,20 +12,40 @@ public class GManager : MonoBehaviour
 
     private Transform _currentBase = null;
     private Transform _currentHead = null;
+    private Transform _currentWings = null;
 
     void Start()
     {
-        if (baseModelPrefabs.Count > 0)
+        MakeModel(0,0,0);
+    }
+
+    public void MakeModel(int baseIndex, int headIndex, int wingIndex)
+    {
+        ClearModel();
+        if (baseIndex < baseModelPrefabs.Count && baseModelPrefabs.Count > 0)
         {
-            _currentBase = Instantiate(
-                baseModelPrefabs[0],
-                target).transform;
+            _currentBase = Instantiate(baseModelPrefabs[0], target).transform;
         }
 
-        if (headModelPrefabs.Count > 0)
+        if (_currentBase && headIndex < headModelPrefabs.Count && headModelPrefabs.Count > 0)
         {
             _currentHead = LocalInstanciate(_currentBase, headModelPrefabs[0], "Head").transform;
         }
+
+        if (_currentBase && wingIndex < wingModelPrefabs.Count && wingModelPrefabs.Count > 0)
+        {
+            _currentWings = LocalInstanciate(_currentBase, wingModelPrefabs[0], "Wings").transform;
+        }
+    }
+
+    private void ClearModel()
+    {
+        foreach (Transform child in transform) {
+            GameObject.Destroy(child.gameObject);
+        }
+        _currentBase = null;
+        _currentHead = null;
+        _currentWings = null;
     }
 
     void Update()
@@ -40,7 +60,8 @@ public class GManager : MonoBehaviour
             Transform _currentChild = parentBase.GetChild(i);
             if (_currentChild.gameObject.name.Contains(childKeyWord))
             {
-                ret = Instantiate(headModelPrefabs[0], _currentChild.transform);
+                ret = Instantiate(prefabToInstanciate, _currentChild.transform);
+                break;
             }
         }
         return ret;
