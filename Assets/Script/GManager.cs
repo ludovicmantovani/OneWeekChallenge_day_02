bfde38272ac93f82c570d38aeb39a81bd0a6fec5
ludovicmantovani <ludovic.mantov@gmail.com>
2023-03-14 17:07:49 +0100

    [ADD] : Paws front and back

--- Assets/Script/GManager.cs
+++ Assets/Script/GManager.cs
@@ -9,32 +9,45 @@ public class GManager : MonoBehaviour
     [SerializeField] private List<GameObject> baseModelPrefabs = null;
     [SerializeField] private List<GameObject> headModelPrefabs = null;
     [SerializeField] private List<GameObject> wingModelPrefabs = null;
+    [SerializeField] private List<GameObject> pawsModelPrefabs = null;
 
     private Transform _currentBase = null;
     private Transform _currentHead = null;
     private Transform _currentWings = null;
+    private Transform _currentFPaws = null;
+    private Transform _currentBPaws = null;
 
     void Start()
     {
-        MakeModel(0,0,0);
+        MakeModel(0,0,0,0,0);
     }
 
-    public void MakeModel(int baseIndex, int headIndex, int wingIndex)
+    public void MakeModel(int baseIndex, int headIndex, int wingIndex, int fpawsIndex, int bpawsIndex)
     {
         ClearModel();
         if (baseIndex < baseModelPrefabs.Count && baseModelPrefabs.Count > 0)
         {
-            _currentBase = Instantiate(baseModelPrefabs[0], target).transform;
+            _currentBase = Instantiate(baseModelPrefabs[baseIndex], target).transform;
         }
 
         if (_currentBase && headIndex < headModelPrefabs.Count && headModelPrefabs.Count > 0)
         {
-            _currentHead = LocalInstanciate(_currentBase, headModelPrefabs[0], "Head").transform;
+            _currentHead = LocalInstanciate(_currentBase, headModelPrefabs[headIndex], "Head").transform;
         }
 
         if (_currentBase && wingIndex < wingModelPrefabs.Count && wingModelPrefabs.Count > 0)
         {
-            _currentWings = LocalInstanciate(_currentBase, wingModelPrefabs[0], "Wings").transform;
+            _currentWings = LocalInstanciate(_currentBase, wingModelPrefabs[wingIndex], "Wings").transform;
+        }
+
+        if (_currentBase && fpawsIndex < pawsModelPrefabs.Count && pawsModelPrefabs.Count > 0)
+        {
+            _currentFPaws = LocalInstanciate(_currentBase, pawsModelPrefabs[fpawsIndex], "FPaws").transform;
+        }
+
+        if (_currentBase && bpawsIndex < pawsModelPrefabs.Count && pawsModelPrefabs.Count > 0)
+        {
+            _currentBPaws = LocalInstanciate(_currentBase, pawsModelPrefabs[bpawsIndex], "BPaws").transform;
         }
     }
 
